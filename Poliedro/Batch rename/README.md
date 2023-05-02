b67933c6d55c2db41c84415ad3b25b38d8e1ed69
Yudi <renanyaguinuma@hotmail.com>
2023-05-01 21:39:07 -0300

    Add Poliedro/Batch rename

---
+++ Poliedro/Batch rename/README.md
@@ -0,0 +1,16 @@
+# Batch rename
+
+Renames .mp4 files from Zoom based on the .csv file provided by the school.
+
+## Usage
+
+```bash
+$ ./batch_rename.sh <folder with .mp4 files>
+```
+
+## input.csv
+
+```csv
+date;teacher;subject;division;title;url
+pt_BR dd/mmm;string;string;string;string;string
+```
---
+++ Poliedro/Batch rename/batch_rename.sh
@@ -0,0 +1,59 @@
+#!/bin/ksh
+
+# Poliedro/Batch rename
+# https://github.com/Yudi/scripts
+
+# Get directory from argument input
+cd "$PWD/$1" || exit
+
+date=()
+# Convert date from dd/mmm to mm-dd
+tail -n +2 ../input.csv | cut -d ';' -f1 |  while IFS="" read -r line; do date+=("$(LANG=pt_BR date -jf "%d/%b" +"%m-%d" "$line")"); done
+
+disciplina=()
+tail -n +2 ../input.csv | cut -d ';' -f3 |  while IFS="" read -r line; do disciplina+=("$line"); done
+
+frente=()
+tail -n +2 ../input.csv | cut -d ';' -f4 |  while IFS="" read -r line; do frente+=("$line"); done
+
+conteudo=()
+tail -n +2 ../input.csv | cut -d ';' -f5 |  while IFS="" read -r line; do conteudo+=("$line"); done
+
+files=(*.mp4)
+filecount=${#files[@]}
+
+# Check if number of files is double to the number of entries in the input.csv file
+if [ $((filecount/2)) -ne ${#date[@]} ]
+then
+    echo "Número de arquivos não corresponde ao número de linhas do arquivo input.csv"
+    exit
+fi
+
+# Replace : with - in the array
+for i in "${!conteudo[@]}"
+do
+    conteudo[i]="${conteudo[$i]//:/ - }"
+done
+
+
+# zsh array starts at 1
+i=1
+j=0
+lado="esq"
+
+for file in *.mp4
+do
+    mv "$file" "${disciplina[$i]} - ${frente[$i]} - ${date[$i]} - ${conteudo[$i]} - $lado.mp4"
+
+    if [ $j -eq 0 ]
+    then
+        lado="dir"
+        j=1
+    elif [ $j -eq 1 ]
+    then
+        lado="esq"
+        j=0
+        i=$((i+1))
+    fi
+
+done
