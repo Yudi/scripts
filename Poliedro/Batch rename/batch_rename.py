d01a62cea2c9a333d62d2cf620df82a60dc81dbe
Yudi <12040397+Yudi@users.noreply.github.com>
2025-04-08 15:05:56 -0300

    Raise exception if CSV is not complete

--- Poliedro/Batch rename/batch_rename.py
+++ Poliedro/Batch rename/batch_rename.py
@@ -3,29 +3,33 @@
 #
 # Usage: python batch_rename.py <data.csv> <directory>
 
-import os, glob
+import os
+import glob
 import csv
-from types import NoneType
 from typing import TypedDict, List
 import dateparser
 import argparse
 
+# Validate if the directory exists
 def dir_path(string):
     if os.path.isdir(string):
         return string
     else:
         raise NotADirectoryError(string)
 
+# Validate if the file exists
 def csv_path(string):
     if os.path.isfile(string):
         return string
     else:
         raise FileNotFoundError(string)
 
+# CLI argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('csvpath', type=csv_path)
 parser.add_argument('directorypath', type=dir_path)
 
+# Define the structure of the CSV data
 class RowData(TypedDict):
     date: str
     disciplina: str
@@ -38,52 +42,56 @@ csvpath = args.csvpath
 directory = args.directorypath
 absolutedirectory = os.path.abspath(directory)
 
-# Get the number of files in the directory
+# Count the number of files in the directory
 num_files = len(glob.glob(f"{absolutedirectory}/[!.]*.mp4"))
 
-# Open the CSV file
-csvfile = open(csvpath, "r")
-
-csvreader = csv.reader(csvfile,delimiter=';')
-next(csvreader)
-
-
-row_count = 0
-
-
+# Read the CSV file and parse the data
 data: List[RowData] = []
-for i, line in enumerate(csvreader):
-    tempRow: RowData = {}
 
-    tempDate= dateparser.parse(line[0], languages=['pt'])
+with open(csvpath, "r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=';')
+    next(csvreader)  # Skip the header
 
-    if type(tempDate) is NoneType:
-        tempRow['date'] = line[0]
-    else:
-        tempRow['date'] = tempDate.strftime("%m-%d")
+    for i, line in enumerate(csvreader):
+        # If line is empty or has less than 5 columns, throw
+        if len(line) < 5:
+            raise Exception(f"Line {i + 1} is empty or has less than 5 columns.")
 
-    tempRow['disciplina'] = line[2]
-    tempRow['frente'] = line[3]
-    tempRow['conteudo'] = line[4].replace("/", "-").replace(":", " -")
-    row_count += 1
+        tempRow: RowData = {}
 
-    data.append(tempRow)
+        tempDate = dateparser.parse(line[0], languages=['pt'])
 
-i = 0
-j = False
-lado = "esq"
+        if tempDate is None:
+            tempRow['date'] = line[0]
+        else:
+            tempRow['date'] = tempDate.strftime("%m-%d")
 
+        tempRow['disciplina'] = line[2]
+        tempRow['frente'] = line[3]
+        tempRow['conteudo'] = line[4].replace("/", "-").replace(":", " -")
+
+        data.append(tempRow)
+
+# Check if the number of files matches the number of rows in the CSV
+row_count = len(data)
 if num_files != row_count * 2:
-    raise Exception("Number of files in the directory and number of rows in the CSV file are not equal")
+    raise Exception(f"Number of files ({num_files}) and number of rows times 2 ({row_count * 2}) do not match.")
+
+# Rename files
+i = 0
+side = "esq"
+isEven = False
 
 for filename in sorted(glob.glob(f"{absolutedirectory}/[!.]*.mp4")):
     info = data[i]
-    os.rename(filename, f"{absolutedirectory}/{info['disciplina']} - {info['frente']} - {info['date']} - {info['conteudo']} - {lado}.mp4") 
+    new_name = f"{info['disciplina']} - {info['frente']} - {info['date']} - {info['conteudo']} - {side}.mp4"
+    new_path = os.path.join(absolutedirectory, new_name)
+    os.rename(filename, new_path)
 
-    if j == False:
-        lado= "dir"
-        j = True
-    elif j == True:
-        lado="esq"
+    if not isEven:
+        side = "dir"
+        isEven = True
+    else:
+        side = "esq"
         i += 1
-        j = False
+        isEven = False
